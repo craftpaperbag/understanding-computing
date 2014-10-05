8a378c52fd05541850bd8a3b7067d8e2550ba851
craftpaperbag <kazu11518@gmail.com>
2014-10-05 15:31:34 +0900

    add IF statement

--- snipets.rb
+++ snipets.rb
@@ -1,4 +1,21 @@
+#
+# 代入の例
+#
+
 Machine.new(
   Assign.new(:x, Add.new(Variable.new(:x), Number.new(1))),
   { x: Number.new(2) }
+).run
+
+#
+# IF文
+#
+
+Machine.new(
+  If.new(
+    Variable.new(:x),
+    Assign.new(:y, Number.new(1)),
+    Assign.new(:y, Number.new(2))
+  ),
+  { x: Boolean.new(true) }
 ).run
